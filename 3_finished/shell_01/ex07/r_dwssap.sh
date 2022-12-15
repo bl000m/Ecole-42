10025b6d7c1e50c0ef6d468334c857e06a9337c2
Mathia Pagani <98359675+bl000m@users.noreply.github.com>
2022-12-15 15:17:31 +0100

    Delete 3_finished directory

--- 3_finished/shell_01/ex07/r_dwssap.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat /etc/passwd | cut -d":" -f 1 |  sed -e '/^[ \t]*#/d' | sed '2n;N;s/.*\n//' | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr "\n" "," | sed 's/,/, /g' | sed 's/\(.*\),/\1./' | tr -d '\n'
